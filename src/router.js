ca841c8d2f6c1f76b4cd62ac6e6be3b044998a70
Jenny Zhang <jenny.zhang@procuraduria.local>
2019-10-14 09:31:25 -0500

    Se puede mostrar los datos y agregar datos

--- src/router.js
+++ src/router.js
@@ -7,8 +7,7 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home
@@ -16,10 +15,12 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/notas',
+      name: 'notas',
+      component: () => import( /* webpackChunkName: "about" */ './views/Notas.vue')
     }
   ]
 })
---
+++ src/views/Notas.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="container">
+    <h1>Notas</h1>
+
+    <b-alert
+      :show="dismissCountDown"
+      dismissible
+      :variant="mensaje.color"
+      @dismissed="dismissCountDown=0"
+      @dismiss-count-down="countDownChanged"
+    >{{this.mensaje.texto}}</b-alert>
+
+    <form @submit.prevent="agregarNota()">
+      <input type="text" placeholder="Nombre" class="form-control my-2" v-model="nota.nombre" />
+      <input
+        type="text"
+        placeholder="Descripcion"
+        class="form-control my-2"
+        v-model="nota.descripcion"
+      />
+      <b-button class="btn-success my-2 btn-block" type="submit">Agregar Nota</b-button>
+    </form>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Descripción</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item,index) in notas" :key="index">
+          <th scope="row">{{item._id}}</th>
+          <td>{{item.nombre}}</td>
+          <td>{{item.descripcion}}</td>
+          <td>
+            <b-button @click="alerta()" variant="info" class="m-1">Acción</b-button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notas: [],
+      dismissSecs: 5,
+      dismissCountDown: 0,
+      mensaje: {
+        color: "",
+        texto: ""
+      },
+      nota: {
+        nombre: "",
+        descripcion: ""
+      }
+    };
+  },
+  created() {
+    this.listarNotas();
+  },
+  methods: {
+    listarNotas() {
+      this.axios
+        .get("/nota")
+        .then(res => {
+          console.log(res.data);
+          this.notas = res.data;
+        })
+        .catch(e => {
+          console.log(e.response);
+        });
+    },
+    alerta() {
+      this.mensaje.color = "danger";
+      this.mensaje.texto = "Probanado Alerta";
+      this.showAlert();
+    },
+    agregarNota() {
+      console.log(this.nota);
+      this.axios
+        .post("/nueva-nota", this.nota)
+        .then(res => {
+          this.notas.push(res.data);
+          this.nota.nombre = "";
+          this.nota.descripcion = "";
+          this.mensaje.color = "success";
+          this.mensaje.texto = "Nota Agregada!";
+          this.showAlert();
+        })
+        .catch(e => {
+          console.log(e.response);
+          console.log(e.response.data.error.errors.nombre.message);
+          if (e.response.data.error.errors.nombre.message) {
+            this.mensaje.texto = e.response.data.error.errors.nombre.message;
+          } else {
+            this.mensaje.texto = "Error de sistema";
+          }
+
+          this.mensaje.color = "danger";
+          this.showAlert();
+        });
+    },
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown;
+    },
+    showAlert() {
+      this.dismissCountDown = this.dismissSecs;
+    }
+  }
+};
+</script>
